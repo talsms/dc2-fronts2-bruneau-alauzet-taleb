dcb4f37f414f24703a51b673820ae67bac5e2074
Sidi Mohammed Taleb <talebsms@hotmail.fr>
2023-04-17 18:08:31 +0200

    Modification du site

--- app2.js
+++ app2.js
@@ -13,12 +13,25 @@ function addToCart(id, price) {
 }
 let addToCartButtons = document.querySelectorAll('.btn');
 
-addToCartButtons.forEach(button => {
-  button.addEventListener('click', function(event) {
+
+addToCartButtons[0].addEventListener('click', function(event) {
     event.preventDefault();
-    let id = button.getAttribute('id').split('-')[3];
-    let price = button.previousElementSibling.textContent;
-    addToCart(id, price);
+    //let id = button.getAttribute('id').split('-')[3];
+    //let price = button.previousElementSibling.textContent;
+    //addToCart(id, price);
+    console.log("teetÒ")
   });
-});
+
+//produit.html
+  // tu recupere le bouton
+  //tu qjoutes un ecouteur d'evenement - commit
+  //recupeqrqtion de lq quantité -
+  //tu creer un objet (id, prix) -
+  //sqve l'objet de  locqlstorqge -
+
+
+//pqnier.html
+  //RECUPERER LES  infos du LS (getItem)
+  //pqrser le json JSON.PARSE -
+  //generer un ligne de pqnier pqr produit present dqsn le LS ----
 
